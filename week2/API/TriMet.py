d916091c0f9580e2e368b642aba6bce93d21fd1a
Zhengmao Zhang <zhengmao@pdx.edu>
2022-04-07 15:48:52 -0700

    finished all asignment of week2

--- week2/API/TriMet.py
+++ week2/API/TriMet.py
@@ -11,6 +11,7 @@
 # Question 1:
 #   What buses are arriving at the PSU campus soon?
 # https://developer.trimet.org/ws/v2/arrivals/locIDs/7774/appid/2B4A2694B832D6CDB45908BDB
+from operator import ge
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -21,6 +22,7 @@ locIDs = '607,13305,10491,5009,12763,7773,'
 s = requests.Session()
 url = f"https://developer.trimet.org/ws/v2/arrivals/minutes/10/locIDs/{locIDs}/7774/appid/{appid}"
 # print(url)
+print(f"Requesting URL : {url}")
 resp = s.get(url)
 soup = BeautifulSoup(resp.text,'html.parser')
 data = json.loads(str(soup))
@@ -30,9 +32,51 @@ for bus in data['resultSet']['arrival']:
     # The number of the direction, either 1 for inbound or 0 for outbound.
     if bus['dir'] == 1:
         buses.add(bus['vehicleID'])
-        print(bus['vehicleID'])
+        # print(bus['vehicleID'])
     # print(bus['vehicleID'])
 # sorted(buses)
+print('What buses are arriving at the PSU campus soon?')
 print(f'{buses} will arrive PSU in 10 mins')
 print(f'Number of Arrival: {len(buses)}')
+# route 12 blockID	:	1202
+# https://developer.trimet.org/ws/v2/vehicles
+print('Do you ride a bus? When is its next arrival?')
+routes = 12
+blockID = 1202
+print(f"Yes, I take buses route {routes} at block {blockID}")
+url = f'https://developer.trimet.org/ws/v2/vehicles/routes/{routes}/blocks/{blockID}/appid/{appid}'
+resp = s.get(url)
+print(f"Requesting URL : {url}")
+soup = BeautifulSoup(resp.text,'html.parser')
+data = json.loads(str(soup))
+print("There is ",len(data['resultSet']['vehicle']), 'Buses will arrive my block')
 
+# https://developer.trimet.org/ws/V1/routeConfig
+print("Which route has the most stops?")
+url = f'https://developer.trimet.org/ws/V1/routeConfig/json/true/stops/true/appid/{appid}'
+print(url)
+resp = s.get(url)
+soup = BeautifulSoup(resp.text,'html.parser')
+data = json.loads(str(soup))
+print(len(data['resultSet']['route']))
+routesNum = set()
+for route in data['resultSet']['route']:
+    routesNum.add(route['id'])
+# print(routesNum)
+maxStop = 0
+maxStopRoute = 0
+for i in routesNum:
+    url = f'https://developer.trimet.org/ws/V1/routeConfig/route/{i}/json/true/dir/true/stops/true/appid/{appid}'
+    print(f"Requesting URL : {url}")
+    resp = s.get(url)
+    soup = BeautifulSoup(resp.text,'html.parser')
+    data = json.loads(str(soup))
+    # print(len(data['resultSet']['route'][0]['dir'][1]))
+    dir1 = data['resultSet']['route'][0]['dir'][0]['stop']
+    dir2 = data['resultSet']['route'][0]['dir'][0]['stop']
+    stops = len(dir1) + len(dir2)
+    if(stops > maxStop):
+        # print(stops)
+        maxStop = stops
+        maxStopRoute = i
+print("The Route ", maxStopRoute ," has the most stops")  
