61cc52f57b9be8d6b7d915c6661392cd86b15ff0
Zhengmao Zhang <zhengmao@pdx.edu>
2022-04-06 20:43:43 -0700

    trying to get all info from trimet api

---
+++ week2/API/TriMet.py
@@ -0,0 +1,10 @@
+# https://developer.trimet.org/ws/V1/arrivals?locIDs=6849,6850&appID=2B4A2694B832D6CDB45908BDB
+# To get a list of every stop considered a time point in direction 1 of route number 70 in json
+# https://developer.trimet.org/ws/V1/routeConfig/route/70/dir/1/tp/true/json/true/appid/2B4A2694B832D6CDB45908BDB
+# To get a list of every stop considered a time point in direction 1 of route number 70
+# https://developer.trimet.org/ws/V1/routeConfig/route/70/dir/1/tp/true/json=true/appid/2B4A2694B832D6CDB45908BDB
+# PSU Stattion Name: PSU Urban Center/SW 6th & Montgomery
+# PSU Stattion ID: 7774
+# PSU Stattion Name: PSU South/SW 6th & College
+# PSU Stattion ID: 10293
+# arrive at 7774 data :https://developer.trimet.org/ws/v2/arrivals/locIDs/7774/appid/2B4A2694B832D6CDB45908BDB
